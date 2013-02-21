999b04b7850ac2c86d9382056f93cb27d4f510d0
James Northrup <northrup.james@gmail.com>
2013-02-21 13:29:48 -0800

    [INFO] BUILD SUCCESS -DskipTests [INFO] Reactor Summary: [INFO] [INFO] gwt-parent ........................................ SUCCESS [0.404s] [INFO] gwt-core .......................................... SUCCESS [2.572s] [INFO] gwt-dev_core ...................................... SUCCESS [19.284s] [INFO] gwt-user .......................................... SUCCESS [32.481s] [INFO] gwt-distro-source ................................. SUCCESS [0.046s] [INFO] gwt-distro-source_core ............................ SUCCESS [0.044s] [INFO] gwt-doc ........................................... SUCCESS [0.045s] [INFO] gwt-reference_Microbenchmarks ..................... SUCCESS [1.409s] [INFO] gwt-tools_api-checker ............................. SUCCESS [2.906s] [INFO] gwt-tools_benchmark-viewer ........................ SUCCESS [1.485s] [INFO] gwt-tools_cldr-import ............................. SUCCESS [2.431s] [INFO] gwt-tools_datetimefmtcreator ...................... SUCCESS [1.054s] [INFO] ------------------------------------------------------------------------
    checkpoint

--- gwt-build_tools_ant_gwt/src/test/java/com/google/gwt/sample/core/linker/SimpleAppCacheLinkerTest.java
+++ gwt-build_tools_ant_gwt/src/test/java/com/google/gwt/sample/core/linker/SimpleAppCacheLinkerTest.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.gwt.sample.core.linker;
+package test.core.linker;
 
 import com.google.gwt.core.ext.LinkerContext;
 import com.google.gwt.core.ext.TreeLogger;
